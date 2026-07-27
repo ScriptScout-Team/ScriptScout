73088d5f0e6511e69faaf67d0db8a80ed6969c33
krishna <rentikotaramaKrishna86@gmail.com>
2026-07-27 19:34:00 +0530

    update karma file for Quality gate pass

--- ScriptScout/karma.conf.js
+++ ScriptScout/karma.conf.js
@@ -34,7 +34,18 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['Chrome'],
+    customLaunchers: {
+  ChromeHeadlessNoSandbox: {
+    base: 'ChromeHeadless',
+    flags: [
+      '--no-sandbox',
+      '--disable-gpu',
+      '--disable-dev-shm-usage'
+    ]
+  }
+},
+
+browsers: ['ChromeHeadlessNoSandbox'],
     restartOnFileChange: true
   });
 };
